b2b485382b2f19f39ae3e059bff99168c29887ee
Sebastian Rabiej <sebastian.rabiej@softwaremill.com>
2025-01-20 17:18:19 +0100

    first commit

---
+++ src/test/java/org/renovate/renovate_for_blog/RenovateForBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.renovate.renovate_for_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenovateForBlogApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
